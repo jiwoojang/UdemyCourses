550f0234ba3309a4d5a55518d4535b6cc78926b0
Jiwooj <jiwooo.jang@gmail.com>
2018-02-25 20:24:21 -0500

    Began work on backend flow, and using Node JS

---
+++ The Web Developer Bootcamp/Unit 22 - Node JS/echo.js
@@ -0,0 +1,11 @@
+// Echo function
+function echo(printString, num) {
+    for (var i = 0; i < num; i++){
+        console.log(printString);
+    }
+}
+
+// Test cases
+echo("Hello!", 10);
+
+echo("Frech fries", 5);
